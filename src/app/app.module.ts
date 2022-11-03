67170753d3786cbfa3c1017458d441163703c2f4
DaniRonda <dani68h5@easv365.dk>
2022-11-03 18:40:09 +0100

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoxDashboardComponent } from './box-dashboard/box-dashboard.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoxDashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/box-dashboard/box-dashboard.model.ts
@@ -0,0 +1,7 @@
+﻿export class BoxModel{
+  id:number=0;
+  size:string ='';
+  material:string='';
+  price:string='';
+  amount:string='';
+}
